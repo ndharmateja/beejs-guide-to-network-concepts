442c09bb74c113b012c759fc774b12f3bc0a3635
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-05 15:24:11 +0530

    add error scenarios and refactor into main function

--- code/project1-http-client-and-server/webclient.py
+++ code/project1-http-client-and-server/webclient.py
@@ -40,27 +40,47 @@ def receive_all_data(s):
     return data
 
 
-# 0. Parse the url and port from the command line args
-url, port = parse_url_and_port()
+def main():
+    # 0. Parse the url and port from the command line args
+    url, port = parse_url_and_port()
 
-# 1. Create a socket
-s = socket.socket()
+    # 1. Create a socket and set timeout
+    s = socket.socket()
+    s.settimeout(10.0)
 
-# 2. Get the IP using DNS (optional in python as the connect()
-# method takes care of it) and connect to the IP + port
-s.connect((url, port))
+    try:
+        # 2. Get the IP using DNS (optional in python as the connect()
+        # method takes care of it) and connect to the IP + port
+        s.connect((url, port))
 
-# 3. Create and send the HTTP request
-# GET / HTTP/1.1
-# Host: <url>
-# Connection: close
-# <blank line>
-bytes = create_http_get_request(url)
-s.sendall(bytes)
+        # 3. Create and send the HTTP request
+        # GET / HTTP/1.1
+        # Host: <url>
+        # Connection: close
+        # <blank line>
+        request_bytes = create_http_get_request(url)
+        s.sendall(request_bytes)
 
-# 4. Receive all the data
-data = receive_all_data(s)
-print(data)
+        # 4. Receive all the data
+        data = receive_all_data(s)
+        print(data)
 
-# 5. Close the socket
-s.close()
+    # Handle error scenarios
+    except socket.gaierror:
+        print(f"Error: Couldn't resolve '{url}'. Check spelling and internet.")
+    except ConnectionRefusedError:
+        print(f"Error: Connection refused on port {port}.")
+    except socket.timeout:
+        print(f"Error: Connection timed out.")
+    except Exception as e:
+        print("Unexpected error: {e}.")
+
+    # Cleanup resources
+    finally:
+        # 5. Close the socket
+        s.close()
+        print("\nSocket closed")
+
+
+if __name__ == "__main__":
+    main()
