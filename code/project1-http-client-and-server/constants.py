9d80c26369b8b32c6fec8669afa8f8f9f4ced7ea
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-11 21:30:23 +0530

    add enums for http methods and content types

--- code/project1-http-client-and-server/constants.py
+++ code/project1-http-client-and-server/constants.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 # General
 CRLF = "\r\n"
 ENCODING = "iso-8859-1"
@@ -9,3 +11,15 @@ DEFAULT_PORT = 80
 # Server
 DEFAULT_LISTENING_PORT = 28333
 DEFAULT_BUFFER_SIZE = 4096
+
+
+class HttpMethod(Enum):
+    GET = "GET"
+    POST = "POST"
+    DELETE = "DELETE"
+
+
+class ContentType(Enum):
+    TEXT_PLAIN = "text/plain"
+    TEXT_HTML = "text/html"
+    JSON = "application/json"
