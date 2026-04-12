0fca7e821986c5c4cc2416fbb3fe362ff51775d9
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-12 16:50:35 +0530

    finish the word server project

--- code/project3-the-word-server/wordclient.py
+++ code/project3-the-word-server/wordclient.py
@@ -12,7 +12,7 @@ def usage():
 packet_buffer = b""
 
 
-def get_next_word_packet(s):
+def get_next_word_packet(s: socket.socket):
     """
     Return the next word packet from the stream.
 
@@ -25,10 +25,41 @@ def get_next_word_packet(s):
 
     global packet_buffer
 
-    # TODO -- Write me!
+    while True:
+        # Check if the buffer already has a full word packet
+        # If the buffer already has a full word packet, we can extract
+        # the word packet from the buffer and return it
+
+        # We want the buffer to have atleast WORD_LEN_SIZE + 1 bytes
+        # because just having WORD_LEN_SIZE bytes means that no bytes are
+        # there for the word in the buffer
+        if len(packet_buffer) > WORD_LEN_SIZE:
+            # Parse the length of the word in the packet
+            word_length = int.from_bytes(packet_buffer[:WORD_LEN_SIZE], "big")
+            packet_length = WORD_LEN_SIZE + word_length
+
+            # If the full packet is present in the buffer, we can extract
+            # and exit
+            if len(packet_buffer) >= packet_length:
+                packet = packet_buffer[:packet_length]
+                packet_buffer = packet_buffer[packet_length:]
+                return packet
+
+        # If we reach here, it means that either there weren't even
+        # WORD_LEN_SIZE + 1 bytes in the buffer or the full packet isn't present
+        # So we receive the next bytes of the stream and go to the next iteration
+        received_bytes = s.recv(4096)
+
+        # If connection is closed then 0 bytes are received, in which case we can
+        # return None
+        if not received_bytes:
+            return None
+
+        # Append the bytes received to the buffer
+        packet_buffer += received_bytes
 
 
-def extract_word(word_packet):
+def extract_word(word_packet: bytes):
     """
     Extract a word from a word packet.
 
@@ -38,7 +69,10 @@ def extract_word(word_packet):
     Returns the word decoded as a string.
     """
 
-    # TODO -- Write me!
+    # The first WORD_LEN_SIZE bytes store the length of the word
+    # Decode the word_length number of bytes as a word (UTF-8 is default)
+    word = word_packet[WORD_LEN_SIZE:].decode()
+    return word
 
 
 # Do not modify:
