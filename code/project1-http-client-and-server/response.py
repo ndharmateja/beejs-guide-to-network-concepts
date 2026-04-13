2d3e12161e89e91dca7230c427ee31204db88757
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-13 16:04:25 +0530

    refactor the separate response and response builder classes into one

--- code/project1-http-client-and-server/response.py
+++ code/project1-http-client-and-server/response.py
@@ -4,6 +4,41 @@ from constants import CRLF, ENCODING, ContentType
 from errors import InvalidResponseException
 
 
+class ResponseBuilder:
+    def __init__(self):
+        self.status_code = Response.DEFAULT_STATUS_CODE
+        self.status_message = Response.DEFAULT_STATUS_MESSAGE
+        self.version = Response.DEFAULT_VERSION
+        self.content = ""
+        self.content_type = Response.DEFAULT_CONTENT_TYPE
+
+    def set_status(self, code: int, message: str):
+        self.status_code = code
+        self.status_message = message
+        return self
+
+    def set_version(self, version: str):
+        self.version = version
+        return self
+
+    def set_content(self, content: str, content_type: ContentType = None):
+        self.content = content
+        if content_type:
+            if not isinstance(content_type, ContentType):
+                raise TypeError("content_type must be a valid ContentType enum")
+            self.content_type = content_type
+        return self
+
+    def build(self) -> "Response":
+        return Response(
+            status_code=self.status_code,
+            status_message=self.status_message,
+            version=self.version,
+            content=self.content,
+            content_type=self.content_type,
+        )
+
+
 class Response:
     DEFAULT_VERSION = "HTTP/1.1"
     DEFAULT_STATUS_CODE = 200
--- code/project1-http-client-and-server/response/__init__.py
+++
@@ -1,3 +0,0 @@
-# To avoid imports like response.response.Response
-from .response import Response
-from .response_builder import ResponseBuilder
