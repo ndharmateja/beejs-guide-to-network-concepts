31f82542b9faabc8d2f694e454ca8b74103221a8
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-04-14 19:27:20 +0530

    add support for accessing nested files and folders

--- code/project1-http-client-and-server/static_file.py
+++ code/project1-http-client-and-server/static_file.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 
-from constants import ENCODING, ContentType
+from constants import BASE_STATIC_DIR, ENCODING, ContentType
 from errors import MimeTypeNotSupportedException
 
 
@@ -15,21 +16,30 @@ class StaticFile:
     }
 
     def __init__(self, file_path):
-        self.base_dir = "static"
+        # Get the absolute path and handle invalid path cases
+        self.server_root = os.path.abspath(BASE_STATIC_DIR)
+        full_path = os.path.sep.join((self.server_root, file_path))
+        full_path = os.path.abspath(full_path)
 
-        if file_path == "/":
-            self.__content_bytes = self.generate_files_listing()
+        # We have to make sure that whatever is being requested is a (nested)
+        # subfolder in the base directory
+        if not full_path.startswith(self.server_root):
+            raise FileNotFoundError("Invalid path.")
+
+        # If a folder is requested, show the listing
+        if os.path.isdir(full_path):
+            self.__content_bytes = self.generate_files_listing(full_path)
             self.__content_type = ContentType.TEXT_HTML
             return
 
+        # If a file is requested, serve it
         # Get the filename
-        file_name = os.path.split(file_path)[-1]
-        file_extension = os.path.splitext(file_name)[-1]
+        file_extension = os.path.splitext(full_path)[-1]
 
         # If no extension, then we assume it is html
         if not file_extension:
             file_extension = ".html"
-            file_name += file_extension
+            full_path += file_extension
 
         # If the file extension is not supported, throw error
         if file_extension not in StaticFile.EXTENSION_TO_MIME_TYPE:
@@ -38,20 +48,29 @@ class StaticFile:
             )
 
         # Open the file and read the contents
-        with open(os.path.join(self.base_dir, file_name), "rb") as f:
+        # Automatically throws a FileNotFoundError if the requested file
+        # doesn't exist
+        with open(full_path, "rb") as f:
             self.__content_bytes = f.read()
             self.__content_type = self.EXTENSION_TO_MIME_TYPE[file_extension]
 
-    def generate_files_listing(self):
-        entries = os.listdir(self.base_dir)
+    def generate_files_listing(self, full_path):
+        """Assumes that full_path is a path to a folder"""
+
+        # Get the relative path of the given folder
+        dir_path = Path(full_path)
+        relative_path = dir_path.relative_to(self.server_root)
 
         # Start building the HTML
         html = f"<html><head><title>Index</title></head><body>"
-        html += f"<h1>Index of /</h1><ul>"
+        html += f"<h1>Index of /{relative_path}</h1><ul>"
 
         # If entry is "cat.jpg", the link should be "/cat.jpg"
+        entries = os.listdir(full_path)
         for entry in entries:
-            html += f'<li><a href="/{entry}">{entry}</a></li>'
+            entry_path = dir_path / entry
+            relative_path = entry_path.relative_to(self.server_root)
+            html += f'<li><a href="/{relative_path}">{entry}</a></li>'
 
         html += "</ul></body></html>"
         return html.encode(ENCODING)
